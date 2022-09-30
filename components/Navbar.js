fbe4efe7a13385f548bd3d79ce0f44e43a244f55
Michael Schulz <mi.schulz@hotmail.com>
2022-09-30 16:36:58 +0200

    Change cart, change styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   const context = useContext(ThemeContext);
 
   return (
-    <nav className="relative flex items-center justify-between max-w-6xl pt-4 pb-1 mx-auto border-b-2 border-black nav dark:text-white dark:border-b-slate-50">
+    <nav className="relative flex items-center justify-between max-w-6xl py-5 mx-auto border-b-2 border-black nav dark:text-white dark:border-b-slate-50">
       <Link href="/" className="text-3xl logo">
         <div className="ml-8 text-3xl cursor-pointer">
           <span className="font-thin ">origa</span>
@@ -39,24 +39,22 @@ const Navbar = () => {
             <a>CONTACT</a>
           </Link>
         </li>
-        <li className="pt-2 nav-item cart">
+        <li className="pb-1 nav-item cart hover:scale-105">
           <Link href="/">
             <svg
-              width="60px"
-              height="60px"
-              fill={context.darkMode === true ? '#fff' : '#000'}
-              viewBox="0 0 96 96"
+              width="35px"
+              height="35px"
+              viewBox="0 0 24 24"
+              fill={context.darkMode ? 'rgb(17 24 39)' : '#fff'}
               xmlns="http://www.w3.org/2000/svg"
             >
-              <title />
-              <g id="Cart">
-                <path d="M35,64a6,6,0,1,0,6,6A6,6,0,0,0,35,64Zm0,11a5,5,0,1,1,5-5A5,5,0,0,1,35,75Z" />
-                <path d="M63,64a6,6,0,1,0,6,6A6,6,0,0,0,63,64Zm0,11a5,5,0,1,1,5-5A5,5,0,0,1,63,75Z" />
-                <path d="M67.79,55a.49.49,0,0,0,.46-.31l11.6-29a.5.5,0,0,0-.46-.69H23.92L23,21.39h0v0a.55.55,0,0,0-.08-.15l0-.05-.08-.07-.07,0-.09,0H16.5a.5.5,0,0,0-.5.5v4a.5.5,0,0,0,.5.5h6.64l8,31.14A3,3,0,0,0,32,63H68.5a.5.5,0,0,0,.5-.5v-5a.5.5,0,0,0-.5-.5H32.15l-.52-2ZM78.65,26,67.45,54H31.62L24.39,26ZM17,25V22h5.11l.75,3ZM68,62H32a2,2,0,0,1,0-4H68Z" />
-                <path d="M41.5,50A2.5,2.5,0,0,0,44,47.5v-15a2.5,2.5,0,0,0-5,0v15A2.5,2.5,0,0,0,41.5,50ZM40,32.5a1.5,1.5,0,0,1,3,0v15a1.5,1.5,0,0,1-3,0Z" />
-                <path d="M50.5,50A2.5,2.5,0,0,0,53,47.5v-15a2.5,2.5,0,0,0-5,0v15A2.5,2.5,0,0,0,50.5,50ZM49,32.5a1.5,1.5,0,0,1,3,0v15a1.5,1.5,0,0,1-3,0Z" />
-                <path d="M59.5,50A2.5,2.5,0,0,0,62,47.5v-15a2.5,2.5,0,0,0-5,0v15A2.5,2.5,0,0,0,59.5,50ZM58,32.5a1.5,1.5,0,0,1,3,0v15a1.5,1.5,0,0,1-3,0Z" />
-              </g>
+              <path
+                d="M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM17 17H7.36729C6.86964 17 6.44772 16.6341 6.37735 16.1414L6.07143 14M2 3H4.5L4.78571 5M4.78571 5H5H21L18 14H6.5H6.07143M4.78571 5L6.07143 14M11 19C11 20.1046 10.1046 21 9 21C7.89543 21 7 20.1046 7 19C7 17.8954 7.89543 17 9 17C10.1046 17 11 17.8954 11 19Z"
+                stroke={context.darkMode ? '#fff' : '#000'}
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
             </svg>
           </Link>
         </li>
