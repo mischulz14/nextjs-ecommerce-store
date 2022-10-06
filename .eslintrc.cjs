77768d1908f90363b040b844750d540e9a3f7003
Michael Schulz <mi.schulz@hotmail.com>
2022-10-06 11:50:03 +0200

    Start migration to typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,24 @@
+require('dotenv-safe').config({
+  path: './.env',
+});
+
 module.exports = {
   extends: ['@upleveled/upleveled'],
+  plugins: [' @ ts - safeql / eslint - plugin '],
+  rules: {
+    ' @ ts - safeql / check - sql ': [
+      ' error ',
+      {
+        connections: [
+          {
+            databaseUrl:
+              ' postgres : // next_js_ecommerce : next_js_ecommerce @ localhost : 5432 / next_js_ecommerce ',
+            tagName: ' sql ',
+            fieldTransform: ' camel ',
+            transform: ' $ { type } [ ] ',
+          },
+        ],
+      },
+    ],
+  },
 };
--- data/connect.ts
+++ data/connect.ts
@@ -3,15 +3,50 @@ import postgres from 'postgres';
 
 config();
 
-const sql = postgres({
-  transform: {
-    ...postgres.camel,
-    undefined: null,
-  },
-});
+// const sql = postgres({
+//   transform: {
+//     ...postgres.camel,
+//     undefined: null,
+//   },
+// });
+
+// Type needed for the connection function below
+declare module globalThis {
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
+}
+
+// Connect only once to the database
+// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+function connectOneTimeToDatabase() {
+  if (!globalThis.postgresSqlClient) {
+    globalThis.postgresSqlClient = postgres({
+      transform: {
+        ...postgres.camel,
+        undefined: null,
+      },
+    });
+  }
+  return globalThis.postgresSqlClient;
+}
+
+// Connect to PostgreSQL
+export const sql = connectOneTimeToDatabase();
+
+export type Origami = {
+  id: number;
+  name: string;
+  price: number;
+  activePrice: number;
+  secondColor: string;
+  firstPicture: string;
+  secondPicture: string;
+  activePicture: string;
+  difficulty: number;
+  count: number;
+};
 
 export async function getOrigamiList() {
-  const origamiFigures = await sql`
+  const origamiFigures = await sql<Origami[]>`
   SELECT * FROM origami`;
   return origamiFigures;
 }
