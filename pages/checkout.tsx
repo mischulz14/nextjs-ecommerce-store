f522eeef85746812f5f63ed19803036aa780062f
Michael Schulz <mi.schulz@hotmail.com>
2022-10-11 20:21:01 +0200

    Add checkout states, change styling

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,14 +1,49 @@
 import Image from 'next/image';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ProductContext } from '../context/ProductContext';
 
 const Checkout = () => {
   const productContext = useContext(ProductContext);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [zip, setZip] = useState('');
+  const [country, setCountry] = useState('');
+  const [creditCard, setCreditCard] = useState('');
+  const [expirationDate, setExpirationDate] = useState('');
+  const [securityCode, setSecurityCode] = useState('');
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log('submit');
+
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !address ||
+      !city ||
+      !zip ||
+      !country ||
+      !creditCard ||
+      !expirationDate ||
+      !securityCode
+    ) {
+      alert('Please fill out all fields');
+      return;
+    }
+  };
+
   return (
     <div className="mx-auto dark:text-white dark:bg-slate-600">
       {/* <span>Checkout</span>
       <span>{productContext.totalPrice}</span> */}
-      <form className="mx-auto checkout__form">
+      <form
+        onSubmit={(event) => handleSubmit(event)}
+        className="mx-auto checkout__form"
+      >
         <div className="flex flex-col justify-center gap-2 mx-auto text-black checkout__form-container sm:flex-row">
           <div className="flex flex-col checkout__form__person-info">
             <div className="checkout__image-wrapper">
@@ -18,16 +53,22 @@ const Checkout = () => {
               Personal Info
             </span>
             <input
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
               data-test-id="checkout-first-name"
               type="text"
               placeholder="First Name"
             />
             <input
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
               data-test-id="checkout-last-name"
               type="text"
               placeholder="Last Name"
             />
             <input
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
               type="email"
               data-test-id="checkout-email"
               placeholder="E-Mail"
@@ -41,16 +82,20 @@ const Checkout = () => {
               Location
             </span>
             <input
+              value={address}
+              onChange={(event) => setAddress(event.target.value)}
               data-test-id="checkout-address"
               type="text"
               placeholder="Address"
             />
             <input
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
               data-test-id="checkout-city"
               type="text"
               placeholder="City"
             />
-            <input type="text" placeholder="State" />
+
             <input
               data-test-id="checkout-postal-code"
               type="text"
@@ -89,7 +134,7 @@ const Checkout = () => {
         </div>
         <button
           data-test-id="checkout-confirm-order"
-          className="mx-auto mb-6 btn-primary bg-slate-900"
+          className="mx-auto mb-6 btn-primary bg-slate-900 "
         >
           Confirm Order
         </button>
