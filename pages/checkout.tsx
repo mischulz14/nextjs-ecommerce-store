738dc4f1e8035ac115aa8cf6787fe733703b0b0c
Michael Schulz <mi.schulz@hotmail.com>
2022-10-11 20:08:49 +0200

    Add checkout-page styling

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -6,62 +6,93 @@ const Checkout = () => {
   const productContext = useContext(ProductContext);
   return (
     <div className="mx-auto dark:text-white dark:bg-slate-600">
-      <span>Checkout</span>
-      <span>{productContext.totalPrice}</span>
-      <form className="flex text-black checkout__form">
-        <div className="flex flex-col border-2 border-black checkout__form__person-info">
-          <Image src="/images/person.svg" width="50" height="50" />
-          <input
-            data-test-id="checkout-first-name"
-            type="text"
-            placeholder="First Name"
-          />
-          <input
-            data-test-id="checkout-last-name"
-            type="text"
-            placeholder="Last Name"
-          />
-          <input
-            type="email"
-            data-test-id="checkout-email"
-            placeholder="E-Mail"
-          />
-        </div>
-        <div className="flex flex-col border-2 border-black checkout__form__person-location">
-          <Image src="/images/house.svg" width="50" height="50" />
-          <input
-            data-test-id="checkout-address"
-            type="text"
-            placeholder="Address"
-          />
-          <input data-test-id="checkout-city" type="text" placeholder="City" />
-          <input type="text" placeholder="State" />
-          <input
-            data-test-id="checkout-postal-code"
-            type="text"
-            placeholder="Postal Code"
-          />
-          <input
-            data-test-id="checkout-country"
-            type="country"
-            placeholder="Country"
-          />
-        </div>
-        <div className="flex flex-col border-2 border-black checkout__form__person-payment-info">
-          <Image src="/images/credit-card.svg" width="50" height="50" />
-          <input
-            id="ccn"
-            type="tel"
-            inputMode="numeric"
-            pattern="[0-9\s]{13,19}"
-            autoComplete="cc-number"
-            maxLength={19}
-            placeholder="xxxx xxxx xxxx xxxx"
-          />
+      {/* <span>Checkout</span>
+      <span>{productContext.totalPrice}</span> */}
+      <form className="mx-auto checkout__form">
+        <div className="flex flex-col justify-center gap-2 mx-auto text-black checkout__form-container sm:flex-row">
+          <div className="flex flex-col checkout__form__person-info">
+            <div className="checkout__image-wrapper">
+              <Image src="/images/person.svg" width="50" height="50" />
+            </div>
+            <span className="mb-4 text-center text-black uppercase dark:text-white">
+              Personal Info
+            </span>
+            <input
+              data-test-id="checkout-first-name"
+              type="text"
+              placeholder="First Name"
+            />
+            <input
+              data-test-id="checkout-last-name"
+              type="text"
+              placeholder="Last Name"
+            />
+            <input
+              type="email"
+              data-test-id="checkout-email"
+              placeholder="E-Mail"
+            />
+          </div>
+          <div className="flex flex-col checkout__form__person-location">
+            <div className="checkout__image-wrapper">
+              <Image src="/images/house.svg" width="50" height="50" />
+            </div>
+            <span className="mb-4 text-center text-black uppercase dark:text-white">
+              Location
+            </span>
+            <input
+              data-test-id="checkout-address"
+              type="text"
+              placeholder="Address"
+            />
+            <input
+              data-test-id="checkout-city"
+              type="text"
+              placeholder="City"
+            />
+            <input type="text" placeholder="State" />
+            <input
+              data-test-id="checkout-postal-code"
+              type="text"
+              placeholder="Postal Code"
+            />
+            <input
+              data-test-id="checkout-country"
+              type="country"
+              placeholder="Country"
+            />
+          </div>
+          <div className="flex flex-col checkout__form__person-payment-info">
+            <div className="checkout__image-wrapper">
+              <Image src="/images/credit-card.svg" width="50" height="50" />
+            </div>
+            <span className="mb-4 text-center text-black uppercase dark:text-white">
+              Payment Details
+            </span>
+            <input
+              id="ccn"
+              type="tel"
+              inputMode="numeric"
+              pattern="[0-9\s]{13,19}"
+              autoComplete="cc-number"
+              maxLength={19}
+              placeholder="xxxx xxxx xxxx xxxx"
+            />
 
-          <input type="date" data-test-id="checkout-expiration-date" />
-          <input type="text" data-test-id="checkout-security-code" />
+            <input type="date" data-test-id="checkout-expiration-date" />
+            <input
+              type="text"
+              data-test-id="checkout-security-code"
+              placeholder="Security Code"
+            />
+          </div>
         </div>
+        <button
+          data-test-id="checkout-confirm-order"
+          className="mx-auto mb-6 btn-primary bg-slate-900"
+        >
+          Confirm Order
+        </button>
       </form>
     </div>
   );
