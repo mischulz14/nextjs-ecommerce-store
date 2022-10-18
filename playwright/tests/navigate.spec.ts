69d0e1b3db0ab3a1fef1201a1695635d1d579d0a
Michael Schulz <mi.schulz@hotmail.com>
2022-10-18 10:29:31 +0200

    Change git attributes

--- playwright/tests/navigate.spec.ts
+++ playwright/tests/navigate.spec.ts
@@ -1,96 +1,96 @@
-// import { expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-// // E2E tests for the app
+// E2E tests for the app
 
-// test('navigate to home page', async ({ page }) => {
-//   await page.goto('http://localhost:3000');
-//   await expect(page.locator('h1')).toHaveText('DO YOU LOVE ORIGAMI?');
+test('navigate to home page', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page.locator('h1')).toHaveText('DO YOU LOVE ORIGAMI?');
 
-//   await page.locator('role=link[name="PRODUCTS"]').click();
-//   await expect(page).toHaveURL('http://localhost:3000/products');
-// });
+  await page.locator('role=link[name="PRODUCTS"]').click();
+  await expect(page).toHaveURL('http://localhost:3000/products');
+});
 
-// test('add to cart button', async ({ page }) => {
-//   await page.goto('http://localhost:3000/products');
-//   await page.locator('.modal-btn').click();
-//   await page
-//     .locator(
-//       'li:has-text("Added to cart!DoveDifficulty: 110$") [data-test-id="product-add-to-cart"]',
-//     )
-//     .click();
+test('add to cart button', async ({ page }) => {
+  await page.goto('http://localhost:3000/products');
+  await page.locator('.modal-btn').click();
+  await page
+    .locator(
+      'li:has-text("Added to cart!DoveDifficulty: 110$") [data-test-id="product-add-to-cart"]',
+    )
+    .click();
 
-//   await expect(page.locator(':nth-match(.cart-test, 1)')).toHaveText('1');
-// });
+  await expect(page.locator(':nth-match(.cart-test, 1)')).toHaveText('1');
+});
 
-// test('test price after adding to cart', async ({ page }) => {
-//   await page.goto('http://localhost:3000/products');
-//   await page.locator('.modal-btn').click();
-//   await page
-//     .locator(
-//       'li:has-text("Added to cart!DoveDifficulty: 110$") [data-test-id="product-add-to-cart"]',
-//     )
-//     .click();
-//   await page.goto('http://localhost:3000/cart');
+test('test price after adding to cart', async ({ page }) => {
+  await page.goto('http://localhost:3000/products');
+  await page.locator('.modal-btn').click();
+  await page
+    .locator(
+      'li:has-text("Added to cart!DoveDifficulty: 110$") [data-test-id="product-add-to-cart"]',
+    )
+    .click();
+  await page.goto('http://localhost:3000/cart');
 
-//   await expect(page.locator('.cart-total')).toHaveText('10');
-// });
+  await expect(page.locator('.cart-total')).toHaveText('10');
+});
 
-// test('test changing quantity in the single product page', async ({ page }) => {
-//   await page.goto('http://localhost:3000/products/19');
-//   await page.locator('.modal-btn').click();
-//   await page.locator('.increase-btn').click();
-//   await page.locator('.add-to-cart-btn').click();
-//   await expect(page.locator(':nth-match(.cart-test, 1)')).toHaveText('2');
-// });
+test('test changing quantity in the single product page', async ({ page }) => {
+  await page.goto('http://localhost:3000/products/19');
+  await page.locator('.modal-btn').click();
+  await page.locator('.increase-btn').click();
+  await page.locator('.add-to-cart-btn').click();
+  await expect(page.locator(':nth-match(.cart-test, 1)')).toHaveText('2');
+});
 
-// test('test changing quantity in the cart', async ({ page }) => {
-//   await page.goto('http://localhost:3000/products');
-//   await page.locator('.modal-btn').click();
-//   await page.locator(':nth-match(.add-to-cart-btn, 1)').click();
-//   await page.goto('http://localhost:3000/cart');
-//   await page.locator('.increase-btn-cart').click();
-//   await expect(page.locator(':nth-match(.cart-test, 1)')).toHaveText('2');
-// });
+test('test changing quantity in the cart', async ({ page }) => {
+  await page.goto('http://localhost:3000/products');
+  await page.locator('.modal-btn').click();
+  await page.locator(':nth-match(.add-to-cart-btn, 1)').click();
+  await page.goto('http://localhost:3000/cart');
+  await page.locator('.increase-btn-cart').click();
+  await expect(page.locator(':nth-match(.cart-test, 1)')).toHaveText('2');
+});
 
-// test('test removing item from cart', async ({ page }) => {
-//   await page.goto('http://localhost:3000/products');
-//   await page.locator('.modal-btn').click();
-//   await page.locator(':nth-match(.add-to-cart-btn, 1)').click();
-//   await page.goto('http://localhost:3000/cart');
-//   await page.locator('.remove-btn').click();
-//   await expect(page.locator('.cart-total')).toHaveText('0');
-// });
+test('test removing item from cart', async ({ page }) => {
+  await page.goto('http://localhost:3000/products');
+  await page.locator('.modal-btn').click();
+  await page.locator(':nth-match(.add-to-cart-btn, 1)').click();
+  await page.goto('http://localhost:3000/cart');
+  await page.locator('.remove-btn').click();
+  await expect(page.locator('.cart-total')).toHaveText('0');
+});
 
-// test('test checkout button', async ({ page }) => {
-//   await page.goto('http://localhost:3000/products');
-//   await page.locator('.modal-btn').click();
-//   await page.locator(':nth-match(.add-to-cart-btn, 1)').click();
-//   await page.goto('http://localhost:3000/cart');
-//   await page.locator('.checkout-btn').click();
-//   await expect(page).toHaveURL('http://localhost:3000/checkout');
-// });
+test('test checkout button', async ({ page }) => {
+  await page.goto('http://localhost:3000/products');
+  await page.locator('.modal-btn').click();
+  await page.locator(':nth-match(.add-to-cart-btn, 1)').click();
+  await page.goto('http://localhost:3000/cart');
+  await page.locator('.checkout-btn').click();
+  await expect(page).toHaveURL('http://localhost:3000/checkout');
+});
 
-// test('test checkout form', async ({ page }) => {
-//   await page.goto('http://localhost:3000/products');
-//   await page.locator('.modal-btn').click();
-//   await page.locator(':nth-match(.add-to-cart-btn, 1)').click();
-//   await page.goto('http://localhost:3000/cart');
-//   await page.locator('.checkout-btn').click();
-//   await page.fill('#first-name', 'test');
-//   await page.fill('#last-name', 'test');
-//   await page.fill('#email', 'test@test.com');
-//   await page.fill('#address', 'test');
-//   await page.fill('#city', 'test');
-//   await page.selectOption('#country', 'Austria');
-//   await page.fill('#zip', 'test');
-//   await page.fill('#ccn', '4242424242424242');
-//   await page.fill('#expiration-date', '1111');
-//   await page.fill('#security-code', '111');
-//   await page.locator('.checkout-confirm-order').click();
-//   await expect(page).toHaveURL('http://localhost:3000/thankyou');
-// });
+test('test checkout form', async ({ page }) => {
+  await page.goto('http://localhost:3000/products');
+  await page.locator('.modal-btn').click();
+  await page.locator(':nth-match(.add-to-cart-btn, 1)').click();
+  await page.goto('http://localhost:3000/cart');
+  await page.locator('.checkout-btn').click();
+  await page.fill('#first-name', 'test');
+  await page.fill('#last-name', 'test');
+  await page.fill('#email', 'test@test.com');
+  await page.fill('#address', 'test');
+  await page.fill('#city', 'test');
+  await page.selectOption('#country', 'Austria');
+  await page.fill('#zip', 'test');
+  await page.fill('#ccn', '4242424242424242');
+  await page.fill('#expiration-date', '1111');
+  await page.fill('#security-code', '111');
+  await page.locator('.checkout-confirm-order').click();
+  await expect(page).toHaveURL('http://localhost:3000/thankyou');
+});
 
-// test('test thank you page', async ({ page }) => {
-//   await page.goto('http://localhost:3000/thankyou');
-//   await expect(page.locator('h1')).toHaveText('Thank you for your order!');
-// });
+test('test thank you page', async ({ page }) => {
+  await page.goto('http://localhost:3000/thankyou');
+  await expect(page.locator('h1')).toHaveText('Thank you for your order!');
+});
--- utils/__tests__/cookie.test.ts
+++ utils/__tests__/cookie.test.ts
@@ -1,31 +1,58 @@
-// import {
-//   getParsedCookie,
-//   handleCookieChange,
-//   removeAllCookies,
-//   setStringifiedCookie,
-// } from '../cookies';
-// import { getTotalCost, getTotalQuantity } from '../getTotal';
+import {
+  getParsedCookie,
+  handleCookieChange,
+  removeAllCookies,
+  setStringifiedCookie,
+} from '../cookies';
+import { getTotalCost, getTotalQuantity } from '../getTotal';
 
-// // test('stringify a cookie value', () => {
-// //   expect(stringifyCookieValue([{ id: '1', stars: 2 }])).toBe(
-// //     '[{"id":"1","stars":2}]',
-// //   );
-// // });
+// test('stringify a cookie value', () => {
+//   expect(stringifyCookieValue([{ id: '1', stars: 2 }])).toBe(
+//     '[{"id":"1","stars":2}]',
+//   );
+// });
 
-// test('set, gets and delete a cookie', () => {
+test('set, gets and delete a cookie', () => {
+  const cookieKey = 'count';
+  const cookieValue = {
+    id: 1,
+    activePicture: '/images/active.png',
+    count: 2,
+  };
+  // First make sure that the return of the function is undefined
+  // Use .toBe to compare primitive values
+  expect(getParsedCookie(cookieKey)).toBe(undefined);
+  // Set the cookie value and test if the value is updated
+  expect(() => setStringifiedCookie(cookieKey, cookieValue)).not.toThrow();
+  // Test cookie value, use .toStrictEqual to test that the object have the same type as well as structure
+  expect(getParsedCookie(cookieKey)).toStrictEqual(cookieValue);
+  // Best practice: clear the state you created after the test to bring the system back to the initial state
+  // Delete cookie
+  expect(removeAllCookies(cookieKey)).toBe(undefined);
+  // Check if delete function was successful
+  expect(getParsedCookie(cookieKey)).toBe(undefined);
+});
+
+// TODO fix this test
+// test('update cookie count', () => {
 //   const cookieKey = 'count';
 //   const cookieValue = {
 //     id: 1,
 //     activePicture: '/images/active.png',
 //     count: 2,
 //   };
-//   // First make sure that the return of the function is undefined
-//   // Use .toBe to compare primitive values
+
 //   expect(getParsedCookie(cookieKey)).toBe(undefined);
-//   // Set the cookie value and test if the value is updated
 //   expect(() => setStringifiedCookie(cookieKey, cookieValue)).not.toThrow();
-//   // Test cookie value, use .toStrictEqual to test that the object have the same type as well as structure
 //   expect(getParsedCookie(cookieKey)).toStrictEqual(cookieValue);
+//   // Update cookie value
+//   expect(() => handleCookieChange(cookieKey, cookieValue, true)).not.toThrow();
+//   // Check if update function was successful
+//   expect(getParsedCookie(cookieKey)).toStrictEqual({
+//     id: 1,
+//     activePicture: '/images/active.png',
+//     count: 3,
+//   });
 //   // Best practice: clear the state you created after the test to bring the system back to the initial state
 //   // Delete cookie
 //   expect(removeAllCookies(cookieKey)).toBe(undefined);
@@ -33,44 +60,17 @@
 //   expect(getParsedCookie(cookieKey)).toBe(undefined);
 // });
 
-// // TODO fix this test
-// // test('update cookie count', () => {
-// //   const cookieKey = 'count';
-// //   const cookieValue = {
-// //     id: 1,
-// //     activePicture: '/images/active.png',
-// //     count: 2,
-// //   };
-
-// //   expect(getParsedCookie(cookieKey)).toBe(undefined);
-// //   expect(() => setStringifiedCookie(cookieKey, cookieValue)).not.toThrow();
-// //   expect(getParsedCookie(cookieKey)).toStrictEqual(cookieValue);
-// //   // Update cookie value
-// //   expect(() => handleCookieChange(cookieKey, cookieValue, true)).not.toThrow();
-// //   // Check if update function was successful
-// //   expect(getParsedCookie(cookieKey)).toStrictEqual({
-// //     id: 1,
-// //     activePicture: '/images/active.png',
-// //     count: 3,
-// //   });
-// //   // Best practice: clear the state you created after the test to bring the system back to the initial state
-// //   // Delete cookie
-// //   expect(removeAllCookies(cookieKey)).toBe(undefined);
-// //   // Check if delete function was successful
-// //   expect(getParsedCookie(cookieKey)).toBe(undefined);
-// // });
-
-// test('cart sum and total price', () => {
-//   const cart = [
-//     {
-//       price: 10,
-//       count: 2,
-//     },
-//     {
-//       price: 10,
-//       count: 3,
-//     },
-//   ];
-//   expect(getTotalQuantity(cart)).toBe(5);
-//   expect(getTotalCost(cart)).toBe(50);
-// });
+test('cart sum and total price', () => {
+  const cart = [
+    {
+      price: 10,
+      count: 2,
+    },
+    {
+      price: 10,
+      count: 3,
+    },
+  ];
+  expect(getTotalQuantity(cart)).toBe(5);
+  expect(getTotalCost(cart)).toBe(50);
+});
